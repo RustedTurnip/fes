d9740cbbd65d8ab7f29f1fc1fd241cc07c14e38c
Samuel Wilcox <samuelwilcox@outlook.com>
2026-04-28 16:49:32 +0100

    fix relative Output path bug
    This commit fixes using the first part of a relative path (e.g.
    ".." from "../foobar") as the output file's package name.

--- schema/template.go
+++ schema/template.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"golang.org/x/mod/modfile"
@@ -57,7 +58,15 @@ type tmplData struct {
 }
 
 func schemaToTemplData(s *Schema) (tmplData, error) {
-	dir, _ := path.Split(s.destination)
+	ap, err := filepath.Abs(s.destination)
+	if err != nil {
+		return tmplData{}, fmt.Errorf(
+			"failed to determine absolute package path: %w",
+			err,
+		)
+	}
+
+	dir, _ := path.Split(ap)
 
 	dst, err := destinationPackage(dir)
 	if err != nil {
